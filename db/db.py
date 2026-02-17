52d6a6d7181f48829a84c380ce192debfd86c37a
abdullah <abdullah.alkhurayjah@gmail.com>
2026-02-17 16:07:36 +0300

    Update db.py

--- db/db.py
+++ db/db.py
@@ -1,5 +1,4 @@
-import streamlit as st
 import psycopg2
-
+import streamlit as st
 def get_connection():
-    return psycopg2.connect(st.secrets["postgresql://postgres:[YOUR-PASSWORD]@db.gddiloxvtcawylknvqnd.supabase.co:5432/postgres"])
+    return psycopg2.connect(st.secrets["postgresql://postgres.gddiloxvtcawylknvqnd:2M&fQw/2PDa#Q#C@aws-1-ap-southeast-1.pooler.supabase.com:5432/postgres"])
