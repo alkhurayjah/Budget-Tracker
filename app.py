e26226ec643822d42ffc7d6c849c6e6f4acf4c70
Abdulmajeed Alshehri <144609898+Abdulmajeed-A@users.noreply.github.com>
2026-02-17 10:45:17 +0300

    testing simple data storage
    testing simple data storage using CSV file

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-print("hi, python")
+import pandas as pd
+import os
+
+# Create file if it doesn't exist
+if not os.path.isfile('expenses.csv'):
+    df = pd.DataFrame(columns=['Date', 'Category', 'Amount', 'User'])
+    df.to_csv('expenses.csv', index=False)
